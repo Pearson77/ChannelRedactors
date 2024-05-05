415a462050f7ed90ba28901f702903ed9cd5c78b
Mikhail <mialal0505@gmail.com>
2024-05-05 19:24:52 +0300

    Refactor

--- models/models.py
+++ models/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String, BigInteger, TIMES
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 
-engine = create_engine('sqlite:///database.db', echo=True)
+engine = create_engine('sqlite:///database.db')
 Base = declarative_base()
 
 
